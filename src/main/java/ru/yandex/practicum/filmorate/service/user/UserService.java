32b6d3c96f0e12b9e51f4ede9aee8f6d6b21f8a0
Ruslan Aliullov <aliullov2016@mail.ru>
2025-04-13 19:19:04 +0300

    Спринт 11

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service.user;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
