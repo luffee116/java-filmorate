83869a500f15741e82ddc7dfc68e7fe1b61cdd7f
Kseniia <kore-ksenia@yandex.ru>
2025-05-28 15:01:28 +0300

    корректировки rowMapper

--- src/main/java/ru/yandex/practicum/filmorate/rowMappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/rowMappers/FilmRowMapper.java
@@ -2,37 +2,34 @@ package ru.yandex.practicum.filmorate.rowMappers;
 
 import org.springframework.jdbc.core.RowMapper;
 import ru.yandex.practicum.filmorate.dto.FilmDto;
-import ru.yandex.practicum.filmorate.dto.GenreDto;
 import ru.yandex.practicum.filmorate.dto.MpaDto;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class FilmRowMapper implements RowMapper<FilmDto> {
-    Set<GenreDto> genres = new HashSet<>();
-    Set<Integer> likes = new HashSet<>();
-    Map<Integer, String> reviews = new HashMap<>();
 
     @Override
     public FilmDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+        MpaDto mpa = MpaDto.builder()
+                .id(rs.getInt("mpa_id"))
+                .name(rs.getString("mpa_name"))
+                .description(rs.getString("mpa_description"))
+                .build();
+
         return FilmDto.builder()
                 .id(rs.getInt("id"))
                 .name(rs.getString("name"))
                 .description(rs.getString("description"))
                 .releaseDate(rs.getDate("release_date").toLocalDate())
                 .duration(rs.getLong("duration"))
-                .mpa(MpaDto.builder()
-                        .id(rs.getInt("mpa_id"))
-                        .name(rs.getString("mpa_name"))
-                        .description(rs.getString("mpa_description"))
-                        .build())
-                .genres(genres)
-                .likes(likes)
-                .review(reviews)
+                .mpa(mpa)
+                .genres(new HashSet<>())
+                .likes(new HashSet<>())
+                .directors(new HashSet<>())
+                .review(new HashMap<>())
                 .build();
     }
 }
